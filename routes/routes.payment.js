aa77257dfb9da64e1cb76f6d0d66910ed8a1444a
Đỗ Phúc Hưng <hsdev.29032004@gmail.com>
2024-09-12 10:17:45 +0700

    Update routes.payment.js

--- routes/routes.payment.js
+++ routes/routes.payment.js
@@ -41,7 +41,7 @@ router.post(
             secretKey: 'K951B6PE1waDMi640xX08PD3vg6EkVlz',
             orderInfo: 'pay with MoMo',
             partnerCode: 'MOMO',
-            redirectUrl: 'http://localhost:3000/',
+            redirectUrl: 'http://meelow.vercel.app/',
             ipnUrl: `${process.env.NGROK}/api/payment/callback`, //chú ý: cần dùng ngrok thì momo mới post đến url này được
             requestType: 'payWithMethod',
             extraData: '',
